6fc072356714c84eea11ebc4effd706d7be8ccc2
kamiya <kamiya.yoshiyuki0604@gmail.com>
2025-05-04 09:59:49 +0900

    app_url

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if uploaded_file is not None:
 
     if st.button("推論開始"):
         with st.spinner("物体検出を実行中..."):
-            response = requests.post("http://127.0.0.1:8000/predict/", files={"file": uploaded_file})
+            response = requests.post("https://yolo-mwsq.onrender.com/predict/", files={"file": uploaded_file})
         if response.status_code == 200:
                 img_base64 = response.json()["image_base64"]
                 img_bytes = base64.b64decode(img_base64)
